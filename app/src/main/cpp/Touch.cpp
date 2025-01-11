5e420c667d209026847f07da17f619e55823d752
Sengeki1 <gamersengeki@gmail.com>
2025-01-11 21:17:53 -0100

    trying to motionEvent

---
+++ app/src/main/cpp/Touch.cpp
@@ -0,0 +1,7 @@
+#include "Touch.h"
+
+glm::vec2 getMotionXY(android_app *app) {
+    for (size_t i = 0; i < app->; i++) {
+
+    }
+}
---
+++ app/src/main/cpp/Touch.h
@@ -0,0 +1,11 @@
+#ifndef SHOOTERGAME_TOUCH_H
+#define SHOOTERGAME_TOUCH_H
+
+#include <android/input.h>
+#include <GLES3/gl3.h>
+#include "Libraries/glm/vec2.hpp"
+#include <game-activity/native_app_glue/android_native_app_glue.h>
+
+static glm::vec2 getMotionXY();
+
+#endif //SHOOTERGAME_TOUCH_H
